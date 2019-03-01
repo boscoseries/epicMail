7c757461350c39754acca8676c70f149d97ec8fb
boscoseries <boscoseries@gmail.com>
2019-03-01 17:37:43 +0100

    added index page

---
+++ js/epicMail.js
@@ -0,0 +1,9 @@
+function toggle() {
+    var menu = document.getElementById('nav');
+    if (menu.style.display === 'none') {
+      menu.style.display = 'flex';
+    }
+    else {
+      return menu.style.display = 'none';
+    }
+  }
